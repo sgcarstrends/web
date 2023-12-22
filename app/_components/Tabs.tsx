f92f8a100a33c27a82f46c8718fea0b6a11920b2
Ru Chern Chong <hello@ruchern.xyz>
2023-12-23 00:58:54 +0800

    Fix URL matching for tabs selection

--- app/_components/Tabs.tsx
+++ app/_components/Tabs.tsx
@@ -1,9 +1,9 @@
 "use client";
 
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import { Icon } from "@mdi/react";
 import classNames from "classnames";
-import { usePathname } from "next/navigation";
 
 interface TabItem {
   title: string;
@@ -17,20 +17,26 @@ interface TabProps {
 
 export const Tabs = ({ tabItems }: TabProps) => {
   const pathname = usePathname();
+  const isActive = (href: string) => {
+    // Special case for the root page
+    if (href === "/" && pathname === "/") {
+      return true;
+    }
+
+    return pathname.startsWith(href) && href !== "/";
+  };
 
   return (
     <div className="flex w-full justify-center border-b border-gray-600 text-center text-gray-400">
       <ul className="-mb-px flex flex-wrap gap-x-2">
         {tabItems.map(({ title, href, icon }) => {
-          const isActive = href === pathname;
-
           return (
             <li key={title}>
               <Link
                 href={href}
                 className={classNames(
                   "group inline-flex items-center justify-center gap-x-2 rounded-t-lg border-b-2 border-transparent p-4 hover:border-gray-900 hover:text-gray-900",
-                  { "border-gray-900 text-gray-900": isActive },
+                  { "border-gray-900 text-gray-900": isActive(href) },
                 )}
               >
                 <Icon path={icon} size={1} aria-hidden={true} />
