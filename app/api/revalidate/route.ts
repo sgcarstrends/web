541667c7656e66a6a9065fdc6913017c3ede9286
Ru Chern Chong <hello@ruchern.xyz>
2024-07-28 03:22:08 +0800

    Clean up revalidate route

--- app/api/revalidate/route.ts
+++ app/api/revalidate/route.ts
@@ -3,12 +3,13 @@ import { revalidateTag } from "next/cache";
 
 export const GET = (req: NextRequest) => {
   const secret = req.nextUrl.searchParams.get("secret");
-  const tags = req.nextUrl.searchParams.get("tags") as string;
-
   if (secret !== process.env.NEXT_PUBLIC_REVALIDATE_TOKEN) {
     return NextResponse.json({ message: "Invalid token!" }, { status: 401 });
   }
 
-  revalidateTag(tags);
-  return NextResponse.json({ revalidated: true, now: Date.now() });
+  const tags = req.nextUrl.searchParams.get("tags");
+  if (tags) {
+    revalidateTag(tags);
+    return NextResponse.json({ revalidated: true, now: Date.now() });
+  }
 };
