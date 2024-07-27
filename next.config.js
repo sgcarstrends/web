86e2bcf71148e5a60ec3b343d0b58826ff15e1c8
Ru Chern Chong <hello@ruchern.xyz>
2024-07-28 02:10:16 +0800

    Add temporary redirects

--- next.config.js
+++ next.config.js
@@ -1,9 +1,22 @@
+const isProd = process.env.NODE_ENV === "production";
+
+let redirects = [];
+if (isProd) {
+  redirects = [
+    { source: "/", destination: "/cars", permanent: false },
+    { source: "/coe", destination: "/coe/prices", permanent: false },
+  ];
+}
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   logging: {
     fetches: {
       fullUrl: true,
     },
+  },
+  async redirects() {
+    return redirects;
   },
 };
 
