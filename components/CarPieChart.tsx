cd053c6bd68a24ab3b9420bb9eaeabd30eb3efab
Ru Chern Chong <hello@ruchern.xyz>
2024-08-10 22:59:45 +0800

    Migrate Car pie chart to shadcn/ui chart

--- components/CarPieChart.tsx
+++ components/CarPieChart.tsx
@@ -1,36 +1,56 @@
 "use client";
 
-import { ResponsivePie } from "@nivo/pie";
+import * as React from "react";
+import { Pie, PieChart } from "recharts";
+import {
+  type ChartConfig,
+  ChartContainer,
+  ChartTooltip,
+  ChartTooltipContent,
+} from "@/components/ui/chart";
 
-export const CarPieChart = ({ data }: any) => {
-  data = Object.entries(data).map(([key, value]) => {
-    return {
-      id: key,
+interface CarPieChartProps {
+  data: Record<string, number>;
+}
+
+interface ChartConfigItem {
+  label: string;
+  color: string;
+}
+
+export const CarPieChart = ({ data }: CarPieChartProps) => {
+  const chartData = Object.entries(data)
+    .filter(([_, value]) => value)
+    .map(([key, value]) => ({
       label: key,
       value,
-    };
-  });
+      fill: `var(--color-${key})`,
+    }));
+
+  const chartConfig = Object.keys(data).reduce<Record<string, ChartConfigItem>>(
+    (acc, key, index) => {
+      acc[key] = {
+        label: key,
+        color: `hsl(var(--chart-${index + 1}))`,
+      };
+
+      return acc;
+    },
+    {},
+  ) satisfies ChartConfig;
 
   return (
-    <div className="h-[300px]">
-      <ResponsivePie
-        data={data}
-        margin={{ top: 40, bottom: 40 }}
-        innerRadius={0.5}
-        padAngle={0.7}
-        cornerRadius={3}
-        activeOuterRadiusOffset={8}
-        colors={{ scheme: "tableau10" }}
-        arcLinkLabelsSkipAngle={1}
-        arcLinkLabelsTextColor="#333333"
-        arcLinkLabelsThickness={2}
-        arcLinkLabelsColor={{ from: "color" }}
-        arcLabelsSkipAngle={10}
-        arcLabelsTextColor={{
-          from: "color",
-          modifiers: [["brighter", 5]],
-        }}
-      />
-    </div>
+    <ChartContainer config={chartConfig} className="min-h-[200px] w-full">
+      <PieChart>
+        <ChartTooltip content={<ChartTooltipContent />} />
+        <Pie
+          data={chartData}
+          dataKey="value"
+          nameKey="label"
+          innerRadius={50}
+          strokeWidth={5}
+        />
+      </PieChart>
+    </ChartContainer>
   );
 };
