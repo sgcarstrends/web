f9fcbb3fb6945d22a8e5db001d2336ecb89b5001
Ru Chern Chong <ruchern.chong@sgcarstrends.com>
2024-08-20 01:54:41 +0800

    Update sitemap
    Relates to #67

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,14 +1,31 @@
-import { SITE_URL } from "@/config";
+import { API_URL, SITE_URL } from "@/config";
+import { fetchApi } from "@/utils/fetchApi";
+import type { Make } from "@/types";
 import type { MetadataRoute } from "next";
 
-const sitemap = (): MetadataRoute.Sitemap => {
+// TODO: Interim solution
+const FUEL_TYPE = ["petrol", "hybrid", "electric", "diesel"];
+
+const sitemap = async (): Promise<MetadataRoute.Sitemap> => {
+  const makes = await fetchApi<Make[]>(`${API_URL}/make`);
+
   return [
     {
       url: SITE_URL,
       lastModified: new Date(),
-      changeFrequency: "yearly",
-      priority: 1,
     },
+    {
+      url: `${SITE_URL}/cars`,
+      lastModified: new Date(),
+    },
+    ...FUEL_TYPE.map((type) => ({
+      url: `${SITE_URL}/cars/${type}`,
+      lastModified: new Date(),
+    })),
+    ...makes.map((make) => ({
+      url: `${SITE_URL}/make/${make}`,
+      lastModified: new Date(),
+    })),
   ];
 };
 
