d8881f95b3dc18b1b55c300e9c00d2912fb090f2
Ru Chern Chong <ruchern.chong@sgcarstrends.com>
2024-09-07 21:18:38 +0800

    Add global state persist to localstorage

--- app/store.ts
+++ app/store.ts
@@ -1,8 +1,9 @@
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 import {
   type COEAction,
-  createCoeSlice,
   type CoeSlice,
+  createCoeSlice,
 } from "@/app/store/coeSlice";
 import {
   createDateSlice,
@@ -13,9 +14,17 @@ import {
 type State = DateState & CoeSlice;
 type Action = DateAction & COEAction;
 
-const useStore = create<State & Action>((...a) => ({
-  ...createDateSlice(...a),
-  ...createCoeSlice(...a),
-}));
+const useStore = create<State & Action, [["zustand/persist", unknown]]>(
+  persist(
+    (...a) => ({
+      ...createDateSlice(...a),
+      ...createCoeSlice(...a),
+    }),
+    {
+      name: "config",
+      partialize: ({ categories }) => ({ categories }),
+    },
+  ),
+);
 
 export default useStore;
