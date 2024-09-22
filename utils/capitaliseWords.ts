9e03aca20bc3e6a8bcc0bc2f5ecd5b04f89851c4
Ru Chern Chong <ruchern.chong@sgcarstrends.com>
2024-09-22 20:24:00 +0800

    Revert capitalise words utils

--- utils/capitaliseWords.ts
+++ utils/capitaliseWords.ts
@@ -1,22 +1,6 @@
-export const capitaliseWords = (input: string) => {
-  const splitRegex = /[\s_/]+/;
-
-  const words = input.split(splitRegex);
-
-  const capitalise = words.map(
-    (word) => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase(),
-  );
-
-  return input.split(splitRegex).reduce((result, word, index) => {
-    result += capitalise[index];
-
-    const separator = input.slice(result.length, result.length + 1);
-    if (separator === "/") {
-      result += separator;
-    } else if (index < words.length - 1) {
-      result += " ";
-    }
-
-    return result;
-  }, "");
-};
+export const capitaliseWords = (str: string) =>
+  str
+    .replace("_", " ")
+    .split(" ")
+    .map((word) => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
+    .join(" ");
