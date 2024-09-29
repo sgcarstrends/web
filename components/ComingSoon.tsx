df7f8aa95c3cc50f8bf1c27b6a2f77cd6b6c8e16
Ru Chern Chong <ruchern.chong@sgcarstrends.com>
2024-09-29 19:22:07 +0800

    Update Coming Soon component

---
+++ components/ComingSoon.tsx
@@ -0,0 +1,12 @@
+import type { PropsWithChildren } from "react";
+
+interface Props extends PropsWithChildren {}
+
+export const ComingSoon = ({ children }: Props) => (
+  <div className="pointer-events-none relative inline-block rounded">
+    {children}
+    <span className="absolute left-0 top-0 -translate-y-1/2 translate-x-1/2 rotate-3 transform text-nowrap rounded bg-blue-100 px-2 py-1 text-xs font-bold text-blue-600">
+      Coming Soon
+    </span>
+  </div>
+);
