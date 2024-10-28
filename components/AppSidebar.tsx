bf716bac1d6214cbe9895854caca4eb2e5e249b1
Ru Chern Chong <ruchern.chong@sgcarstrends.com>
2024-10-29 01:17:05 +0800

    Temporary remove sidebar footer

--- components/AppSidebar.tsx
+++ components/AppSidebar.tsx
@@ -142,34 +142,34 @@ export const AppSidebar = () => (
         </SidebarMenu>
       </SidebarGroup>
     </SidebarContent>
-    <SidebarFooter>
-      <SidebarMenu>
-        <SidebarMenuItem>
-          <DropdownMenu>
-            <DropdownMenuTrigger asChild>
-              <SidebarMenuButton>
-                <User2 /> Username
-                <ChevronUp className="ml-auto" />
-              </SidebarMenuButton>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent
-              side="top"
-              className="w-[--radix-popper-anchor-width]"
-            >
-              <DropdownMenuItem>
-                <span>Account</span>
-              </DropdownMenuItem>
-              <DropdownMenuItem>
-                <span>Billing</span>
-              </DropdownMenuItem>
-              <DropdownMenuItem>
-                <span>Sign out</span>
-              </DropdownMenuItem>
-            </DropdownMenuContent>
-          </DropdownMenu>
-        </SidebarMenuItem>
-      </SidebarMenu>
-    </SidebarFooter>
+    {/*<SidebarFooter>*/}
+    {/*  <SidebarMenu>*/}
+    {/*    <SidebarMenuItem>*/}
+    {/*      <DropdownMenu>*/}
+    {/*        <DropdownMenuTrigger asChild>*/}
+    {/*          <SidebarMenuButton>*/}
+    {/*            <User2 /> Username*/}
+    {/*            <ChevronUp className="ml-auto" />*/}
+    {/*          </SidebarMenuButton>*/}
+    {/*        </DropdownMenuTrigger>*/}
+    {/*        <DropdownMenuContent*/}
+    {/*          side="top"*/}
+    {/*          className="w-[--radix-popper-anchor-width]"*/}
+    {/*        >*/}
+    {/*          <DropdownMenuItem>*/}
+    {/*            <span>Account</span>*/}
+    {/*          </DropdownMenuItem>*/}
+    {/*          <DropdownMenuItem>*/}
+    {/*            <span>Billing</span>*/}
+    {/*          </DropdownMenuItem>*/}
+    {/*          <DropdownMenuItem>*/}
+    {/*            <span>Sign out</span>*/}
+    {/*          </DropdownMenuItem>*/}
+    {/*        </DropdownMenuContent>*/}
+    {/*      </DropdownMenu>*/}
+    {/*    </SidebarMenuItem>*/}
+    {/*  </SidebarMenu>*/}
+    {/*</SidebarFooter>*/}
   </Sidebar>
 );
 
