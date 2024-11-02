94850238d6d4555040dddb057c0fb349f3fc36de
Ru Chern Chong <ruchern.chong@sgcarstrends.com>
2024-11-03 05:09:52 +0800

    Update sidebar with correct URLs

--- components/AppSidebar.tsx
+++ components/AppSidebar.tsx
@@ -1,39 +1,26 @@
 "use client";
 
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import {
   Collapsible,
   CollapsibleContent,
   CollapsibleTrigger,
 } from "@radix-ui/react-collapsible";
 import {
-  BookOpen,
-  Bot,
-  Calendar,
+  Battery,
   ChevronRight,
-  ChevronUp,
-  Frame,
-  Home,
-  Inbox,
-  Search,
-  Settings,
-  Settings2,
-  SquareTerminal,
-  User2,
+  DollarSign,
+  Droplet,
+  Fuel,
+  type LucideIcon,
+  Zap,
 } from "lucide-react";
 
-import { PieChart } from "recharts";
 import { BrandLogo } from "@/components/BrandLogo";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
 import {
   Sidebar,
   SidebarContent,
-  SidebarFooter,
   SidebarGroup,
   SidebarGroupLabel,
   SidebarHeader,
@@ -45,237 +32,227 @@ import {
   SidebarMenuSubItem,
 } from "@/components/ui/sidebar";
 
-// Menu items.
-const items = [
-  {
-    title: "Home",
-    url: "#",
-    icon: Home,
-  },
-  {
-    title: "Inbox",
-    url: "#",
-    icon: Inbox,
-  },
-  {
-    title: "Calendar",
-    url: "#",
-    icon: Calendar,
-  },
-  {
-    title: "Search",
-    url: "#",
-    icon: Search,
-  },
-  {
-    title: "Settings",
-    url: "#",
-    icon: Settings,
-  },
-];
+type NavItem = {
+  title: string;
+  icon?: LucideIcon;
+  url: string;
+  isActive?: boolean;
+  items?: NavSubItem[];
+};
 
-export const AppSidebar = () => (
-  <Sidebar>
-    <SidebarHeader>
-      <SidebarMenu>
-        <SidebarMenuItem>
-          <SidebarMenuButton size="lg" asChild>
-            <Link href="/">
-              <div className="flex flex-col gap-0.5 leading-none">
-                <BrandLogo />
-              </div>
-            </Link>
-          </SidebarMenuButton>
-        </SidebarMenuItem>
-      </SidebarMenu>
-    </SidebarHeader>
-    <SidebarContent>
-      <SidebarGroup>
-        <SidebarGroupLabel>Platform</SidebarGroupLabel>
+type NavSubItem = {
+  title: string;
+  icon?: LucideIcon;
+  url: string;
+};
+
+type Nav = {
+  cars: NavItem[];
+  coe: NavItem[];
+};
+
+export const AppSidebar = () => {
+  const pathname = usePathname();
+
+  return (
+    <Sidebar>
+      <SidebarHeader>
         <SidebarMenu>
-          {data.navMain.map((item) => (
-            <Collapsible
-              key={item.title}
-              asChild
-              defaultOpen={item.isActive}
-              className="group/collapsible"
-            >
-              <SidebarMenuItem>
-                <CollapsibleTrigger asChild>
-                  <SidebarMenuButton tooltip={item.title}>
+          <SidebarMenuItem>
+            <SidebarMenuButton size="lg" asChild>
+              <Link href="/">
+                <div className="flex flex-col gap-0.5 leading-none">
+                  <BrandLogo />
+                </div>
+              </Link>
+            </SidebarMenuButton>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarHeader>
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupLabel>Cars</SidebarGroupLabel>
+          <SidebarMenu>
+            {data.cars.map(({ items, ...item }) => (
+              <Collapsible
+                key={item.title}
+                asChild
+                defaultOpen={item.isActive}
+                className="group/collapsible"
+              >
+                <SidebarMenuItem>
+                  <CollapsibleTrigger asChild>
+                    <SidebarMenuButton tooltip={item.title}>
+                      {item.icon && <item.icon />}
+                      <span>{item.title}</span>
+                      <ChevronRight className="ml-auto transition-transform duration-200 group-data-[state=open]/collapsible:rotate-90" />
+                    </SidebarMenuButton>
+                  </CollapsibleTrigger>
+                  <CollapsibleContent>
+                    <SidebarMenuSub>
+                      {items?.map((subItem) => (
+                        <SidebarMenuSubItem key={subItem.title}>
+                          <SidebarMenuSubButton
+                            asChild
+                            isActive={subItem.url === pathname}
+                          >
+                            <Link href={subItem.url}>
+                              {subItem.icon && <subItem.icon />}
+                              <span>{subItem.title}</span>
+                            </Link>
+                          </SidebarMenuSubButton>
+                        </SidebarMenuSubItem>
+                      ))}
+                    </SidebarMenuSub>
+                  </CollapsibleContent>
+                </SidebarMenuItem>
+              </Collapsible>
+            ))}
+          </SidebarMenu>
+        </SidebarGroup>
+        <SidebarGroup className="group-data-[collapsible=icon]:hidden">
+          <SidebarGroupLabel>COE</SidebarGroupLabel>
+          <SidebarMenu>
+            {data.coe.map(({ items, ...item }) => (
+              <SidebarMenuItem key={item.title}>
+                <SidebarMenuButton asChild isActive={item.url === pathname}>
+                  <Link href={item.url}>
                     {item.icon && <item.icon />}
                     <span>{item.title}</span>
-                    <ChevronRight className="ml-auto transition-transform duration-200 group-data-[state=open]/collapsible:rotate-90" />
-                  </SidebarMenuButton>
-                </CollapsibleTrigger>
-                <CollapsibleContent>
-                  <SidebarMenuSub>
-                    {item.items?.map((subItem) => (
-                      <SidebarMenuSubItem key={subItem.title}>
-                        <SidebarMenuSubButton asChild>
-                          <Link href={subItem.url}>
-                            <span>{subItem.title}</span>
-                          </Link>
-                        </SidebarMenuSubButton>
-                      </SidebarMenuSubItem>
-                    ))}
-                  </SidebarMenuSub>
-                </CollapsibleContent>
+                  </Link>
+                </SidebarMenuButton>
+                <SidebarMenuSub>
+                  {items?.map((subItem) => (
+                    <SidebarMenuSubItem key={subItem.title}>
+                      <SidebarMenuSubButton
+                        asChild
+                        isActive={subItem.url === pathname}
+                      >
+                        <Link href={subItem.url}>
+                          {subItem.icon && <subItem.icon />}
+                          <span>{subItem.title}</span>
+                        </Link>
+                      </SidebarMenuSubButton>
+                    </SidebarMenuSubItem>
+                  ))}
+                </SidebarMenuSub>
               </SidebarMenuItem>
-            </Collapsible>
-          ))}
-        </SidebarMenu>
-      </SidebarGroup>
-      <SidebarGroup className="group-data-[collapsible=icon]:hidden">
-        <SidebarGroupLabel>Projects</SidebarGroupLabel>
-        <SidebarMenu>
-          {data.projects.map((item) => (
-            <SidebarMenuItem key={item.name}>
-              <SidebarMenuButton asChild>
-                <Link href={item.url}>
-                  {/*<item.icon />*/}
-                  <span>{item.name}</span>
-                </Link>
-              </SidebarMenuButton>
-            </SidebarMenuItem>
-          ))}
-        </SidebarMenu>
-      </SidebarGroup>
-    </SidebarContent>
-    {/*<SidebarFooter>*/}
-    {/*  <SidebarMenu>*/}
-    {/*    <SidebarMenuItem>*/}
-    {/*      <DropdownMenu>*/}
-    {/*        <DropdownMenuTrigger asChild>*/}
-    {/*          <SidebarMenuButton>*/}
-    {/*            <User2 /> Username*/}
-    {/*            <ChevronUp className="ml-auto" />*/}
-    {/*          </SidebarMenuButton>*/}
-    {/*        </DropdownMenuTrigger>*/}
-    {/*        <DropdownMenuContent*/}
-    {/*          side="top"*/}
-    {/*          className="w-[--radix-popper-anchor-width]"*/}
-    {/*        >*/}
-    {/*          <DropdownMenuItem>*/}
-    {/*            <span>Account</span>*/}
-    {/*          </DropdownMenuItem>*/}
-    {/*          <DropdownMenuItem>*/}
-    {/*            <span>Billing</span>*/}
-    {/*          </DropdownMenuItem>*/}
-    {/*          <DropdownMenuItem>*/}
-    {/*            <span>Sign out</span>*/}
-    {/*          </DropdownMenuItem>*/}
-    {/*        </DropdownMenuContent>*/}
-    {/*      </DropdownMenu>*/}
-    {/*    </SidebarMenuItem>*/}
-    {/*  </SidebarMenu>*/}
-    {/*</SidebarFooter>*/}
-  </Sidebar>
-);
+            ))}
+          </SidebarMenu>
+        </SidebarGroup>
+      </SidebarContent>
+      {/*<SidebarFooter>*/}
+      {/*  <SidebarMenu>*/}
+      {/*    <SidebarMenuItem>*/}
+      {/*      <DropdownMenu>*/}
+      {/*        <DropdownMenuTrigger asChild>*/}
+      {/*          <SidebarMenuButton>*/}
+      {/*            <User2 /> Username*/}
+      {/*            <ChevronUp className="ml-auto" />*/}
+      {/*          </SidebarMenuButton>*/}
+      {/*        </DropdownMenuTrigger>*/}
+      {/*        <DropdownMenuContent*/}
+      {/*          side="top"*/}
+      {/*          className="w-[--radix-popper-anchor-width]"*/}
+      {/*        >*/}
+      {/*          <DropdownMenuItem>*/}
+      {/*            <span>Account</span>*/}
+      {/*          </DropdownMenuItem>*/}
+      {/*          <DropdownMenuItem>*/}
+      {/*            <span>Billing</span>*/}
+      {/*          </DropdownMenuItem>*/}
+      {/*          <DropdownMenuItem>*/}
+      {/*            <span>Sign out</span>*/}
+      {/*          </DropdownMenuItem>*/}
+      {/*        </DropdownMenuContent>*/}
+      {/*      </DropdownMenu>*/}
+      {/*    </SidebarMenuItem>*/}
+      {/*  </SidebarMenu>*/}
+      {/*</SidebarFooter>*/}
+    </Sidebar>
+  );
+};
 
-const data = {
-  navMain: [
+// TODO: Slugify URLs
+const data: Nav = {
+  cars: [
     {
-      title: "Playground",
-      url: "#",
-      icon: SquareTerminal,
+      title: "Fuel Type",
+      url: "/cars/fuel-type",
       isActive: true,
       items: [
         {
-          title: "History",
-          url: "#",
+          title: "Petrol",
+          icon: Fuel,
+          url: "/cars/fuel-type/petrol",
         },
         {
-          title: "Starred",
-          url: "#",
+          title: "Hybrid",
+          icon: Zap,
+          url: "/cars/fuel-type/hybrid",
         },
         {
-          title: "Settings",
-          url: "#",
-        },
-      ],
-    },
-    {
-      title: "Models",
-      url: "#",
-      icon: Bot,
-      items: [
-        {
-          title: "Genesis",
-          url: "#",
+          title: "Electric",
+          icon: Battery,
+          url: "/cars/fuel-type/electric",
         },
         {
-          title: "Explorer",
-          url: "#",
-        },
-        {
-          title: "Quantum",
-          url: "#",
+          title: "Diesel",
+          icon: Droplet,
+          url: "/cars/fuel-type/diesel",
         },
       ],
     },
     {
-      title: "Documentation",
-      url: "#",
-      icon: BookOpen,
+      title: "Vehicle Type",
+      url: "/cars/vehicle-type",
+      isActive: true,
       items: [
         {
-          title: "Introduction",
-          url: "#",
+          title: "Hatchback",
+          url: "/cars/vehicle-type/hatchback",
         },
         {
-          title: "Get Started",
-          url: "#",
+          title: "Sedan",
+          url: "/cars/vehicle-type/sedan",
         },
         {
-          title: "Tutorials",
-          url: "#",
+          title: "Multi-purpose Vehicle",
+          url: `/cars/vehicle-type/${encodeURIComponent("multi-purpose vehicle")}`,
         },
         {
-          title: "Changelog",
-          url: "#",
-        },
-      ],
-    },
-    {
-      title: "Settings",
-      url: "#",
-      icon: Settings2,
-      items: [
-        {
-          title: "General",
-          url: "#",
+          title: "Station-wagon",
+          url: "/cars/vehicle-type/station-wagon",
         },
         {
-          title: "Team",
-          url: "#",
+          title: "Sports Utility Vehicle",
+          url: `/cars/vehicle-type/${encodeURIComponent("sports utility vehicle")}`,
         },
         {
-          title: "Billing",
-          url: "#",
-        },
-        {
-          title: "Limits",
-          url: "#",
+          title: "Coupe/Convertible",
+          url: `/cars/vehicle-type/${encodeURIComponent("coupe/convertible")}`,
         },
       ],
     },
   ],
-  projects: [
+  coe: [
     {
-      name: "Design Engineering",
-      url: "#",
-      icon: Frame,
+      title: "Prices",
+      icon: DollarSign,
+      url: "/coe",
     },
-    {
-      name: "Sales & Marketing",
-      url: "#",
-      icon: PieChart,
-    },
-    {
-      name: "Travel",
-      url: "#",
-      icon: Map,
-    },
+    // {
+    //   title: "COE",
+    //   icon: "",
+    //   url: "/coe",
+    //   isActive: true,
+    //   items: [
+    //     {
+    //       title: "Prices",
+    //       icon: DollarSign,
+    //       url: "/coe/prices",
+    //     },
+    //   ] satisfies NavSubItem[],
+    // },
   ],
 };
