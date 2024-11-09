4cc7f552ea7cba9f3f0c039679ab4039b7becdce
Ru Chern Chong <ruchern.chong@sgcarstrends.com>
2024-11-09 18:30:25 +0800

    Add temporary redirects

--- next.config.js
+++ next.config.js
@@ -5,6 +5,25 @@ if (isProd) {
   redirects = [{ source: "/", destination: "/cars", permanent: false }];
 }
 
+// Temporary redirects until features in the respective page is completed
+redirects = redirects.concat([
+  {
+    source: "/cars/fuel-types",
+    destination: "/cars",
+    permanent: false,
+  },
+  {
+    source: "/cars/vehicle-types",
+    destination: "/cars",
+    permanent: false,
+  },
+  {
+    source: "/cars/makes",
+    destination: "/cars",
+    permanent: false,
+  },
+]);
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   logging: {
