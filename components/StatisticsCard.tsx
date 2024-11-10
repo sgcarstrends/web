223164f978ead1c411c9a691d9e7fe3c9e98857e
Ru Chern Chong <ruchern.chong@sgcarstrends.com>
2024-11-10 23:59:05 +0800

    Clean up details in statistics card

--- components/StatisticsCard.tsx
+++ components/StatisticsCard.tsx
@@ -64,53 +64,51 @@ export const StatisticsCard = ({
       <CardContent>
         <div className="grid grid-cols-1 gap-4">
           <DistributionPieChart data={data} type={title} />
-          <div>
-            <Table>
-              <TableCaption>Click on the rows for more details</TableCaption>
-              <TableHeader>
-                <TableRow>
-                  <TableHead>Type</TableHead>
-                  <TableHead>Count</TableHead>
-                  <TableHead>Distribution</TableHead>
-                </TableRow>
-              </TableHeader>
-              <TableBody>
-                {Object.entries(data)
-                  .filter(([_, value]) => value)
-                  .sort(([_A, numberA], [_B, numberB]) => numberB - numberA)
-                  .map(([key, value], index) => (
-                    <TableRow
-                      key={key}
-                      className="cursor-pointer"
-                      onClick={() => handleRowClick(key)}
-                    >
-                      <TableCell>
-                        <div className="flex items-center gap-2">{key}</div>
-                      </TableCell>
-                      <TableCell className="flex gap-1 font-semibold text-primary">
-                        {value}
-                      </TableCell>
-                      <TableCell>
-                        <Badge variant={getBadgeVariant(value)}>
-                          {formatPercent(value / total, {
-                            minimumFractionDigits: 2,
-                          })}
-                        </Badge>
-                      </TableCell>
-                      <TableCell>
-                        <ArrowRight className="h-4 w-4 text-primary" />
-                      </TableCell>
-                    </TableRow>
-                  ))}
-              </TableBody>
-            </Table>
-            {Object.keys(data).includes(FUEL_TYPE.OTHERS) && (
-              <p className="text-sm text-muted-foreground">
-                Note: We do not know what is the Land Transport Authority&apos;s
-                exact definition of &quot;Others&quot;.
-              </p>
-            )}
-          </div>
+          {Object.keys(data).includes(FUEL_TYPE.OTHERS) && (
+            <p className="text-sm italic text-muted-foreground">
+              Note: We do not know what is the Land Transport Authority&apos;s
+              exact definition of &quot;Others&quot;.
+            </p>
+          )}
+          <Table>
+            <TableCaption>Click on each row for more details</TableCaption>
+            <TableHeader>
+              <TableRow>
+                <TableHead>Type</TableHead>
+                <TableHead>Count</TableHead>
+                <TableHead>Distribution</TableHead>
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              {Object.entries(data)
+                .filter(([_, value]) => value)
+                .sort(([_A, numberA], [_B, numberB]) => numberB - numberA)
+                .map(([key, value], index) => (
+                  <TableRow
+                    key={key}
+                    className="cursor-pointer"
+                    onClick={() => handleRowClick(key)}
+                  >
+                    <TableCell>
+                      <div className="flex flex-col gap-2">{key}</div>
+                    </TableCell>
+                    <TableCell className="flex gap-1 font-semibold text-primary">
+                      {value}
+                    </TableCell>
+                    <TableCell>
+                      <Badge variant={getBadgeVariant(value)}>
+                        {formatPercent(value / total, {
+                          minimumFractionDigits: 2,
+                        })}
+                      </Badge>
+                    </TableCell>
+                    <TableCell>
+                      <ArrowRight className="h-4 w-4 text-primary" />
+                    </TableCell>
+                  </TableRow>
+                ))}
+            </TableBody>
+          </Table>
         </div>
       </CardContent>
     </Card>
