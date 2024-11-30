fc5f5836b291bb57d82e7273dab1e3041cc15488
Ru Chern Chong <ruchern.chong@sgcarstrends.com>
2024-11-30 22:14:47 +0800

    Refactor and clean up components

--- app/components/NavMenu.tsx
+++ app/components/NavMenu.tsx
@@ -104,137 +104,135 @@ export const NavMenu = () => (
 );
 
 export const DesktopNavMenu = () => (
-  <div className="container hidden items-center justify-between p-4 lg:flex">
-    <nav className="flex items-center gap-x-4">
-      <Link href="/" className="flex items-center gap-x-2">
-        <BrandLogo />
-      </Link>
-      <NavMenu />
-    </nav>
-    <UnreleasedFeature>
-      <div className="flex items-center gap-x-4">
-        <div className="relative">
-          <Input
-            type="search"
-            placeholder="Search..."
-            className="border border-gray-300 pl-10 focus:border-transparent focus:outline-none focus:ring-2 focus:ring-blue-500"
-          />
-          <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
-            <Search className="h-4 w-4 text-gray-400" />
-          </div>
-        </div>
-      </div>
-    </UnreleasedFeature>
-  </div>
+  <nav className="container p-4">
+    <Link href="/">
+      <BrandLogo />
+    </Link>
+    {/*<NavMenu />*/}
+    {/*<UnreleasedFeature>*/}
+    {/*  <div className="flex items-center gap-x-4">*/}
+    {/*    <div className="relative">*/}
+    {/*      <Input*/}
+    {/*        type="search"*/}
+    {/*        placeholder="Search..."*/}
+    {/*        className="border border-gray-300 pl-10 focus:border-transparent focus:outline-none focus:ring-2 focus:ring-blue-500"*/}
+    {/*      />*/}
+    {/*      <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">*/}
+    {/*        <Search className="h-4 w-4 text-gray-400" />*/}
+    {/*      </div>*/}
+    {/*    </div>*/}
+    {/*  </div>*/}
+    {/*</UnreleasedFeature>*/}
+  </nav>
 );
 
 export const MobileNavMenu = () => {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <div className="flex items-center justify-between px-4 py-3 lg:hidden">
+    <div className="flex items-center justify-between p-4 lg:hidden">
       <Link href="/" className="flex items-center gap-x-2">
         <BrandLogo />
       </Link>
-      <Sheet open={isOpen} onOpenChange={setIsOpen}>
-        <SheetTrigger asChild>
-          <Button variant="ghost" size="icon">
-            <Menu className="h-6 w-6" />
-          </Button>
-        </SheetTrigger>
-        <SheetContent side="left" className="w-full">
-          <Link
-            href="/"
-            className="mb-6 flex items-center gap-x-2"
-            onClick={() => setIsOpen(false)}
-          >
-            <BrandLogo />
-          </Link>
-          <ScrollArea className="h-full pb-6">
-            <div className="mb-6">
-              <UnreleasedFeature>
-                <div className="relative mb-6">
-                  <Search className="absolute left-3 top-1/2 -translate-y-1/2 transform text-gray-400" />
-                  <Input
-                    type="search"
-                    placeholder="Search cars..."
-                    className="rounded-full border-gray-300 py-2 pl-10 pr-4 focus:border-blue-500 focus:ring focus:ring-blue-200 focus:ring-opacity-50"
-                  />
-                </div>
-              </UnreleasedFeature>
-              <Tabs defaultValue="cars">
-                <TabsList>
-                  <TabsTrigger value="cars">Cars</TabsTrigger>
-                  <TabsTrigger value="coe">COE</TabsTrigger>
-                </TabsList>
-                <ScrollArea className="h-3/4">
-                  <TabsContent value="cars">
-                    <Link href="/cars" onClick={() => setIsOpen(false)}>
-                      <Typography.H4 className="flex items-center gap-1">
-                        Monthly <ArrowRight className="h-6 w-6" />
-                      </Typography.H4>
-                    </Link>
-                    <Accordion type="multiple">
-                      <AccordionItem value="fuelType">
-                        <AccordionTrigger>Fuel Type</AccordionTrigger>
-                        <AccordionContent>
-                          <div className="flex flex-col space-y-4">
-                            {FUEL_TYPE_LINKS.map(
-                              ({ label, description, href, icon: Icon }) => (
-                                <Link
-                                  key={label}
-                                  href={href}
-                                  onClick={() => setIsOpen(false)}
-                                >
-                                  <div className="flex items-center gap-2">
-                                    {Icon && <Icon className="h-4 w-4" />}
-                                    <div>{label}</div>
-                                  </div>
-                                  <Typography.Muted>
-                                    {description}
-                                  </Typography.Muted>
-                                </Link>
-                              ),
-                            )}
-                          </div>
-                        </AccordionContent>
-                      </AccordionItem>
-                      <AccordionItem value="vehicleType">
-                        <AccordionTrigger>Vehicle Type</AccordionTrigger>
-                        <AccordionContent>
-                          <div className="flex flex-col space-y-4">
-                            {VEHICLE_TYPE_LINKS.map(({ label, href }) => (
-                              <Link
-                                key={label}
-                                href={href}
-                                onClick={() => setIsOpen(false)}
-                              >
-                                {label}
-                              </Link>
-                            ))}
-                          </div>
-                        </AccordionContent>
-                      </AccordionItem>
-                    </Accordion>
-                  </TabsContent>
-                  <TabsContent value="coe">
-                    <Accordion type="multiple">
-                      <AccordionItem value="dashboard">
-                        <AccordionTrigger>COE</AccordionTrigger>
-                        <AccordionContent>
-                          <Link href="/coe" onClick={() => setIsOpen(false)}>
-                            Dashboard
-                          </Link>
-                        </AccordionContent>
-                      </AccordionItem>
-                    </Accordion>
-                  </TabsContent>
-                </ScrollArea>
-              </Tabs>
-            </div>
-          </ScrollArea>
-        </SheetContent>
-      </Sheet>
+      {/*<Sheet open={isOpen} onOpenChange={setIsOpen}>*/}
+      {/*  <SheetTrigger asChild>*/}
+      {/*    <Button variant="ghost" size="icon">*/}
+      {/*      <Menu className="h-6 w-6" />*/}
+      {/*    </Button>*/}
+      {/*  </SheetTrigger>*/}
+      {/*  <SheetContent side="left" className="w-full">*/}
+      {/*    <Link*/}
+      {/*      href="/"*/}
+      {/*      className="mb-6 flex items-center gap-x-2"*/}
+      {/*      onClick={() => setIsOpen(false)}*/}
+      {/*    >*/}
+      {/*      <BrandLogo />*/}
+      {/*    </Link>*/}
+      {/*    <ScrollArea className="h-full pb-6">*/}
+      {/*      <div className="mb-6">*/}
+      {/*        <UnreleasedFeature>*/}
+      {/*          <div className="relative mb-6">*/}
+      {/*            <Search className="absolute left-3 top-1/2 -translate-y-1/2 transform text-gray-400" />*/}
+      {/*            <Input*/}
+      {/*              type="search"*/}
+      {/*              placeholder="Search cars..."*/}
+      {/*              className="rounded-full border-gray-300 py-2 pl-10 pr-4 focus:border-blue-500 focus:ring focus:ring-blue-200 focus:ring-opacity-50"*/}
+      {/*            />*/}
+      {/*          </div>*/}
+      {/*        </UnreleasedFeature>*/}
+      {/*        <Tabs defaultValue="cars">*/}
+      {/*          <TabsList>*/}
+      {/*            <TabsTrigger value="cars">Cars</TabsTrigger>*/}
+      {/*            <TabsTrigger value="coe">COE</TabsTrigger>*/}
+      {/*          </TabsList>*/}
+      {/*          <ScrollArea className="h-3/4">*/}
+      {/*            <TabsContent value="cars">*/}
+      {/*              <Link href="/cars" onClick={() => setIsOpen(false)}>*/}
+      {/*                <Typography.H4 className="flex items-center gap-1">*/}
+      {/*                  Monthly <ArrowRight className="h-6 w-6" />*/}
+      {/*                </Typography.H4>*/}
+      {/*              </Link>*/}
+      {/*              <Accordion type="multiple">*/}
+      {/*                <AccordionItem value="fuelType">*/}
+      {/*                  <AccordionTrigger>Fuel Type</AccordionTrigger>*/}
+      {/*                  <AccordionContent>*/}
+      {/*                    <div className="flex flex-col space-y-4">*/}
+      {/*                      {FUEL_TYPE_LINKS.map(*/}
+      {/*                        ({ label, description, href, icon: Icon }) => (*/}
+      {/*                          <Link*/}
+      {/*                            key={label}*/}
+      {/*                            href={href}*/}
+      {/*                            onClick={() => setIsOpen(false)}*/}
+      {/*                          >*/}
+      {/*                            <div className="flex items-center gap-2">*/}
+      {/*                              {Icon && <Icon className="h-4 w-4" />}*/}
+      {/*                              <div>{label}</div>*/}
+      {/*                            </div>*/}
+      {/*                            <Typography.Muted>*/}
+      {/*                              {description}*/}
+      {/*                            </Typography.Muted>*/}
+      {/*                          </Link>*/}
+      {/*                        ),*/}
+      {/*                      )}*/}
+      {/*                    </div>*/}
+      {/*                  </AccordionContent>*/}
+      {/*                </AccordionItem>*/}
+      {/*                <AccordionItem value="vehicleType">*/}
+      {/*                  <AccordionTrigger>Vehicle Type</AccordionTrigger>*/}
+      {/*                  <AccordionContent>*/}
+      {/*                    <div className="flex flex-col space-y-4">*/}
+      {/*                      {VEHICLE_TYPE_LINKS.map(({ label, href }) => (*/}
+      {/*                        <Link*/}
+      {/*                          key={label}*/}
+      {/*                          href={href}*/}
+      {/*                          onClick={() => setIsOpen(false)}*/}
+      {/*                        >*/}
+      {/*                          {label}*/}
+      {/*                        </Link>*/}
+      {/*                      ))}*/}
+      {/*                    </div>*/}
+      {/*                  </AccordionContent>*/}
+      {/*                </AccordionItem>*/}
+      {/*              </Accordion>*/}
+      {/*            </TabsContent>*/}
+      {/*            <TabsContent value="coe">*/}
+      {/*              <Accordion type="multiple">*/}
+      {/*                <AccordionItem value="dashboard">*/}
+      {/*                  <AccordionTrigger>COE</AccordionTrigger>*/}
+      {/*                  <AccordionContent>*/}
+      {/*                    <Link href="/coe" onClick={() => setIsOpen(false)}>*/}
+      {/*                      Dashboard*/}
+      {/*                    </Link>*/}
+      {/*                  </AccordionContent>*/}
+      {/*                </AccordionItem>*/}
+      {/*              </Accordion>*/}
+      {/*            </TabsContent>*/}
+      {/*          </ScrollArea>*/}
+      {/*        </Tabs>*/}
+      {/*      </div>*/}
+      {/*    </ScrollArea>*/}
+      {/*  </SheetContent>*/}
+      {/*</Sheet>*/}
     </div>
   );
 };
--- app/maintenance/layout.tsx
+++ app/maintenance/layout.tsx
@@ -1,9 +1,19 @@
 import type { ReactNode } from "react";
 import { Inter } from "next/font/google";
 import classNames from "classnames";
+import type { Metadata } from "next";
 import "../globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Site Maintenance",
+  description: "Site is currently under maintenance",
+  robots: {
+    index: false,
+    follow: false,
+  },
+};
 
 const layout = ({ children }: { children: ReactNode }) => {
   return (
