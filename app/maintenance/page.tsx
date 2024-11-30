5670b906c75438e0584c201a1b8e6407cc636025
Ru Chern Chong <ruchern.chong@sgcarstrends.com>
2024-11-30 21:05:12 +0800

    Add middleware to control maintenance mode

--- app/maintenance/page.tsx
+++ app/maintenance/page.tsx
@@ -1,7 +1,39 @@
+"use client";
+
+import { useEffect } from "react";
+import { useRouter, useSearchParams } from "next/navigation";
 import { Maintenance } from "@/components/Maintenance";
 
 const MaintenancePage = () => {
-  // Building it this way as I may add stuff to it later
+  const router = useRouter();
+  const searchParams = useSearchParams();
+
+  useEffect(() => {
+    const checkMaintenance = async () => {
+      try {
+        // TODO: Simulating this for now
+        const isMaintenanceMode = process.env.MAINTENANCE_MODE;
+
+        if (!isMaintenanceMode) {
+          const from = searchParams.get("from");
+          if (from) {
+            router.replace(decodeURIComponent(from));
+          } else {
+            router.replace("/");
+          }
+        }
+      } catch (e) {
+        console.error("Error checking maintenance status:", e);
+      }
+    };
+
+    const interval = setInterval(checkMaintenance, 5000);
+
+    void checkMaintenance();
+
+    return () => clearInterval(interval);
+  }, [router, searchParams]);
+
   return <Maintenance />;
 };
 
--- middleware.ts
+++ middleware.ts
@@ -2,15 +2,27 @@ import { NextRequest, NextResponse } from "next/server";
 import { DOMAIN_NAME } from "@/config";
 
 export const middleware = (request: NextRequest) => {
+  // TODO: Temporary method for now
+  const isMaintenanceMode = process.env.MAINTENANCE_MODE === "true";
+
+  if (
+    isMaintenanceMode &&
+    !request.nextUrl.pathname.startsWith("/maintenance")
+  ) {
+    const maintenanceUrl = new URL("/maintenance", request.url);
+    maintenanceUrl.searchParams.set("from", request.url);
+    return NextResponse.redirect(maintenanceUrl);
+  }
+
   const nonce = Buffer.from(crypto.randomUUID()).toString("base64");
   const cspHeader = `
-    default-src 'self';
-    script-src 'self' 'unsafe-eval' 'unsafe-inline' *.${DOMAIN_NAME} *.googletagmanager.com;
-    style-src 'self' 'unsafe-inline';
-    img-src 'self' blob: data:;
-    connect-src *;
-    font-src 'self';
-`;
+      default-src 'self';
+      script-src 'self' 'unsafe-eval' 'unsafe-inline' *.${DOMAIN_NAME} *.googletagmanager.com;
+      style-src 'self' 'unsafe-inline';
+      img-src 'self' blob: data:;
+      connect-src *;
+      font-src 'self';
+  `;
 
   const requestHeaders = new Headers(request.headers);
   requestHeaders.set("x-nonce", nonce);
@@ -41,5 +53,7 @@ export const middleware = (request: NextRequest) => {
 };
 
 export const config = {
-  matcher: "/:path*",
+  matcher: [
+    "/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)",
+  ],
 };
