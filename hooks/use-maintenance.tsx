f33300fb3d1f6a2191861f9c2f7c1da1ba6c3068
Ru Chern CHONG <ruchern.chong@sgcarstrends.com>
2025-03-09 18:57:02 +0800

    Refactor maintenance page and component
    Added a hook to handle the maintenance logic too

---
+++ hooks/use-maintenance.tsx
@@ -0,0 +1,35 @@
+import { useEffect } from "react";
+import { useRouter, useSearchParams } from "next/navigation";
+
+const useMaintenance = (pollingInterval = 5000) => {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+
+  useEffect(() => {
+    const checkMaintenance = async () => {
+      try {
+        // TODO: Simulating this for now
+        const isMaintenanceMode = process.env.MAINTENANCE_MODE;
+
+        if (!isMaintenanceMode) {
+          const from = searchParams.get("from");
+          if (from) {
+            router.replace(decodeURIComponent(from));
+          } else {
+            router.replace("/");
+          }
+        }
+      } catch (e) {
+        console.error("Error checking maintenance status:", e);
+      }
+    };
+
+    const interval = setInterval(checkMaintenance, pollingInterval);
+
+    void checkMaintenance();
+
+    return () => clearInterval(interval);
+  }, [pollingInterval, router, searchParams]);
+};
+
+export default useMaintenance;
