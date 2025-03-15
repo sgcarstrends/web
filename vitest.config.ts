1509096e5dc770b33a2a18e6cdcfc7638d9aabd3
Ru Chern CHONG <ruchern.chong@sgcarstrends.com>
2025-03-15 19:23:16 +0800

    Setup vitest

---
+++ vitest.config.ts
@@ -0,0 +1,11 @@
+import react from "@vitejs/plugin-react";
+import tsconfigPaths from "vite-tsconfig-paths";
+import { configDefaults, defineConfig } from "vitest/config";
+
+export default defineConfig({
+  plugins: [tsconfigPaths(), react()],
+  test: {
+    environment: "jsdom",
+    exclude: [...configDefaults.exclude, "e2e"],
+  },
+});
