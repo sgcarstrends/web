ea972c41395f015ec7a402a6db718808d5d276ff
Ru Chern CHONG <ruchern.chong@sgcarstrends.com>
2025-03-22 00:16:17 +0800

    Restore code

--- components/COEPremiumChart.tsx
+++ components/COEPremiumChart.tsx
@@ -1,21 +1,16 @@
 "use client";
 
-import {
-  type CSSProperties,
-  // useEffect,
-  // useMemo,
-  // useState,
-} from "react";
-// import { usePathname, useRouter } from "next/navigation";
+import { type CSSProperties, useEffect, useMemo, useState } from "react";
+import { usePathname, useRouter } from "next/navigation";
 import { numberFormat } from "@ruchernchong/number-format";
-// import { parse, subMonths, subYears } from "date-fns";
-// import { CalendarIcon } from "lucide-react";
+import { parse, subMonths, subYears } from "date-fns";
+import { CalendarIcon } from "lucide-react";
 import { CartesianGrid, Label, Line, LineChart, XAxis, YAxis } from "recharts";
 import useStore from "@/app/store";
 import {
   Card,
   CardContent,
-  // CardDescription,
+  CardDescription,
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
@@ -26,19 +21,15 @@ import {
   ChartTooltip,
   ChartTooltipContent,
 } from "@/components/ui/chart";
-// import {
-//   Select,
-//   SelectContent,
-//   SelectItem,
-//   SelectTrigger,
-//   SelectValue,
-// } from "@/components/ui/select";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 import { formatDateToMonthYear } from "@/utils/formatDateToMonthYear";
-import type {
-  COEBiddingResult,
-  // COECategory,
-  Month,
-} from "@/types";
+import type { COEBiddingResult, COECategory, Month } from "@/types";
 
 interface Props {
   data: COEBiddingResult[];
@@ -50,129 +41,123 @@ interface TimeRange {
   label: string;
 }
 
-// const LAST_12_MONTHS = (30 * 12).toString();
-// const LAST_5_YEARS = (5 * 30 * 12).toString();
-// const LAST_10_YEARS = (10 * 30 * 12).toString();
+const LAST_12_MONTHS = (30 * 12).toString();
+const LAST_5_YEARS = (5 * 30 * 12).toString();
+const LAST_10_YEARS = (10 * 30 * 12).toString();
 
-export const COEPremiumChart = ({
-  data,
-  // months
-}: Props) => {
-  // const router = useRouter();
-  // const pathname = usePathname();
+export const COEPremiumChart = ({ data, months }: Props) => {
+  const router = useRouter();
+  const pathname = usePathname();
 
   const categories = useStore(({ categories }) => categories);
 
-  // const [timeRange, setTimeRange] = useState(LAST_12_MONTHS);
+  const [timeRange, setTimeRange] = useState(LAST_12_MONTHS);
 
-  // const latestMonth = months[0];
-  // const earliestMonth = months[months.length - 1];
+  const latestMonth = months[0];
+  const earliestMonth = months[months.length - 1];
 
-  // TODO: Tidy up
-  // useEffect(() => {
-  //   const params = new URLSearchParams();
-  //
-  //   const formatMonth = (date: Date) => {
-  //     const year = date.getFullYear();
-  //     const month = ("0" + date.getMonth()).slice(-2);
-  //     return `${year}-${month}`;
-  //   };
-  //
-  //   switch (timeRange) {
-  //     case LAST_12_MONTHS:
-  //       params.append(
-  //         "from",
-  //         `${formatMonth(subMonths(parse(latestMonth, "yyyy-MM", new Date()), 12))}`,
-  //       );
-  //       params.append("to", latestMonth);
-  //       break;
-  //     case LAST_5_YEARS:
-  //       params.append(
-  //         "from",
-  //         `${formatMonth(subYears(parse(latestMonth, "yyyy-MM", new Date()), 5))}`,
-  //       );
-  //       params.append("to", latestMonth);
-  //       break;
-  //     case LAST_10_YEARS:
-  //       params.append(
-  //         "from",
-  //         `${formatMonth(subYears(parse(latestMonth, "yyyy-MM", new Date()), 10))}`,
-  //       );
-  //       params.append("to", latestMonth);
-  //       break;
-  //     case "YTD":
-  //       params.append("from", `${new Date().getFullYear()}-01`);
-  //       params.append("to", latestMonth);
-  //       break;
-  //     case "ALL":
-  //       params.append("from", earliestMonth);
-  //       params.append("to", latestMonth);
-  //       break;
-  //     default:
-  //   }
-  //
-  //   router.push(`${pathname}?${params.toString()}`);
-  // }, [earliestMonth, latestMonth, pathname, router, timeRange]);
+  useEffect(() => {
+    const params = new URLSearchParams();
 
-  // const filteredData: COEBiddingResult[] = useMemo(
-  //   () =>
-  //     data.map((item) =>
-  //       Object.entries(item).reduce((acc: any, [key, value]) => {
-  //         if (
-  //           key === "month" ||
-  //           (key.startsWith("Category") && categories[key as COECategory])
-  //         ) {
-  //           acc[key] = value;
-  //         }
-  //
-  //         return acc;
-  //       }, {}),
-  //     ),
-  //   [categories, data],
-  // );
+    const formatMonth = (date: Date) => {
+      const year = date.getFullYear();
+      const month = ("0" + date.getMonth()).slice(-2);
+      return `${year}-${month}`;
+    };
 
-  const filteredData = data.sort((a, b) => a.month.localeCompare(b.month));
+    switch (timeRange) {
+      case LAST_12_MONTHS:
+        params.append(
+          "from",
+          `${formatMonth(subMonths(parse(latestMonth, "yyyy-MM", new Date()), 12))}`,
+        );
+        params.append("to", latestMonth);
+        break;
+      case LAST_5_YEARS:
+        params.append(
+          "from",
+          `${formatMonth(subYears(parse(latestMonth, "yyyy-MM", new Date()), 5))}`,
+        );
+        params.append("to", latestMonth);
+        break;
+      case LAST_10_YEARS:
+        params.append(
+          "from",
+          `${formatMonth(subYears(parse(latestMonth, "yyyy-MM", new Date()), 10))}`,
+        );
+        params.append("to", latestMonth);
+        break;
+      case "YTD":
+        params.append("from", `${new Date().getFullYear()}-01`);
+        params.append("to", latestMonth);
+        break;
+      case "ALL":
+        params.append("from", earliestMonth);
+        params.append("to", latestMonth);
+        break;
+      default:
+    }
+
+    router.push(`${pathname}?${params.toString()}`);
+  }, [earliestMonth, latestMonth, pathname, router, timeRange]);
+
+  const filteredData: COEBiddingResult[] = useMemo(
+    () =>
+      data.map((item) =>
+        Object.entries(item).reduce((acc: any, [key, value]) => {
+          if (
+            key === "month" ||
+            (key.startsWith("Category") && categories[key as COECategory])
+          ) {
+            acc[key] = value;
+          }
+
+          return acc;
+        }, {}),
+      ),
+    [categories, data],
+  );
 
   const chartConfig = {} satisfies ChartConfig;
 
-  // const TIME_RANGES: TimeRange[] = [
-  //   { timeRange: LAST_12_MONTHS, label: "Last 12 Months" },
-  //   { timeRange: LAST_5_YEARS, label: "Last 5 Years" },
-  //   { timeRange: LAST_10_YEARS, label: "Last 10 Years" },
-  //   { timeRange: "YTD", label: "Year to Date" },
-  //   { timeRange: "ALL", label: "All Time" },
-  // ];
+  const TIME_RANGES: TimeRange[] = [
+    { timeRange: LAST_12_MONTHS, label: "Last 12 Months" },
+    { timeRange: LAST_5_YEARS, label: "Last 5 Years" },
+    { timeRange: LAST_10_YEARS, label: "Last 10 Years" },
+    { timeRange: "YTD", label: "Year to Date" },
+    { timeRange: "ALL", label: "All Time" },
+  ];
 
   return (
     <Card>
       <CardHeader className="flex items-center gap-2 space-y-0 border-b py-5 sm:flex-row">
         <div className="grid flex-1 gap-1 text-center sm:text-left">
           <CardTitle>Quota Premium ($)</CardTitle>
-          {/*<CardDescription>*/}
-          {/*  {`Showing ${TIME_RANGES.find((range) => range.timeRange === timeRange)?.label.toLowerCase()} of COE prices`}*/}
-          {/*</CardDescription>*/}
+          <CardDescription>
+            {`Showing ${TIME_RANGES.find((range) => range.timeRange === timeRange)?.label.toLowerCase()} of COE prices`}
+          </CardDescription>
         </div>
-        {/*<div>*/}
-        {/*  <Select value={timeRange} onValueChange={setTimeRange}>*/}
-        {/*    <SelectTrigger className="rounded-lg sm:ml-auto">*/}
-        {/*      <SelectValue placeholder="Last 12 months" />*/}
-        {/*    </SelectTrigger>*/}
-        {/*    <SelectContent className="rounded-xl">*/}
-        {/*      {TIME_RANGES.map(({ timeRange, label }) => (*/}
-        {/*        <SelectItem*/}
-        {/*          key={timeRange}*/}
-        {/*          value={timeRange}*/}
-        {/*          className="rounded-lg"*/}
-        {/*        >*/}
-        {/*          <div className="flex items-center rounded-lg">*/}
-        {/*            <CalendarIcon className="mr-2 size-4" />*/}
-        {/*            {label}*/}
-        {/*          </div>*/}
-        {/*        </SelectItem>*/}
-        {/*      ))}*/}
-        {/*    </SelectContent>*/}
-        {/*  </Select>*/}
-        {/*</div>*/}
+        <div>
+          <Select value={timeRange} onValueChange={setTimeRange}>
+            <SelectTrigger className="rounded-lg sm:ml-auto">
+              <SelectValue placeholder="Last 12 months" />
+            </SelectTrigger>
+            <SelectContent className="rounded-xl">
+              {TIME_RANGES.map(({ timeRange, label }) => (
+                <SelectItem
+                  key={timeRange}
+                  value={timeRange}
+                  className="rounded-lg"
+                >
+                  <div className="flex items-center rounded-lg">
+                    <CalendarIcon className="mr-2 size-4" />
+                    {label}
+                  </div>
+                </SelectItem>
+              ))}
+            </SelectContent>
+          </Select>
+        </div>
       </CardHeader>
       <CardContent className="p-6">
         <ChartContainer config={chartConfig} className="h-[400px] w-full">
