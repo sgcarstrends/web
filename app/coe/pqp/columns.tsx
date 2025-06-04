2e911998b1c7e983ddb4b8e42eeeeb88a95da0e4
Ru Chern CHONG <ruchern.chong@sgcarstrends.com>
2025-06-05 03:07:50 +0800

    Dedupe columns in PQP

--- app/coe/pqp/columns.tsx
+++ app/coe/pqp/columns.tsx
@@ -4,29 +4,16 @@ import { formatCurrency } from "@/utils/format-currency";
 import type { PQP } from "@/types";
 import type { ColumnDef } from "@tanstack/react-table";
 
+const createCategoryColumn = (category: string): ColumnDef<PQP> => ({
+  accessorKey: category,
+  header: category,
+  cell: ({ row }) => `S${formatCurrency(row.getValue<number>(category))}`,
+});
+
 export const columns: ColumnDef<PQP>[] = [
   {
     accessorKey: "month",
     header: "Month",
   },
-  {
-    accessorKey: "Category A",
-    header: "Category A",
-    cell: ({ row }) => `S${formatCurrency(row.getValue<number>("Category A"))}`,
-  },
-  {
-    accessorKey: "Category B",
-    header: "Category B",
-    cell: ({ row }) => `S${formatCurrency(row.getValue<number>("Category B"))}`,
-  },
-  {
-    accessorKey: "Category C",
-    header: "Category C",
-    cell: ({ row }) => `S${formatCurrency(row.getValue<number>("Category C"))}`,
-  },
-  {
-    accessorKey: "Category D",
-    header: "Category D",
-    cell: ({ row }) => `S${formatCurrency(row.getValue<number>("Category D"))}`,
-  },
+  ...["Category A", "Category B", "Category C", "Category D"].map(createCategoryColumn),
 ];
