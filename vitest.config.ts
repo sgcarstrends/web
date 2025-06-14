a889be3253e80020b41fee63068ecc5a964fd175
Ru Chern CHONG <ruchern.chong@sgcarstrends.com>
2025-06-14 22:51:10 +0800

    Update vitest coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -6,6 +6,17 @@ import { configDefaults, defineConfig } from "vitest/config";
 export default defineConfig({
   plugins: [tsconfigPaths(), react()],
   test: {
+    coverage: {
+      include: ["src"],
+      exclude: [
+        "src/components/ui",
+        "src/config",
+        "src/functions",
+        "src/lib",
+        "src/schema",
+        "src/types",
+      ],
+    },
     environment: "jsdom",
     exclude: [...configDefaults.exclude, "tests"],
     setupFiles: "./setupTests.ts",
